7b917f0b5ea0a2e7f2a608ea168c445b8df02b10
skolk <sean@3scan.com>
2018-04-03 10:29:36 -0400

    keyword error fix

--- make100/Day2Dict/DataDict.py
+++ make100/Day2Dict/DataDict.py
@@ -2,11 +2,13 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "This word doesn't exist"
 
 userInput = 0
 while userInput == 0:
     userInput = input("What word would you like to look up?:")
-    DataReturn = data[userInput]
     print(translate(userInput))
     userInput = 0
