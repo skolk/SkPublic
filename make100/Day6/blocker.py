7d226bc9bb0e777cc4f5464c9cbeda38a835be96
skolk <sean@3scan.com>
2018-05-10 11:47:32 +0200

    blocker complete

--- make100/Day6/blocker.py
+++ make100/Day6/blocker.py
@@ -2,13 +2,31 @@ import time
 from datetime import datetime as dt
 import os
 
-
+#hosts_temp="hosts"
+working_hours=[14,17] #24 hour clock
 hosts_path="/etc/hosts"
 redirect="127.0.0.1"
-website_list=["www.facebook.com","facebook.com","gmail.com","www.gmail.com"]
+website_list=["www.facebook.com","facebook.com","gmail.com","www.gmail.com",www.reddit.com,reddit.com]
 
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
-        print(1)
-        time.sleep(5)
+    if dt(dt.now().year,dt.now().month,dt.now().day,working_hours[0]) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,working_hours[1]):
+        print("working hours")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            #print(content)
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("go forth and prospoer")
+    time.sleep(5)
